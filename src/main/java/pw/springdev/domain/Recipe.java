9cb3f55b8f62ee742871fb1683c63cdf0c497c47
FedorD <fedord.vn@gmail.com>
2020-04-04 13:29:28 +0700

    Refactoring recipe bootstrap

--- src/main/java/pw/springdev/domain/Recipe.java
+++ src/main/java/pw/springdev/domain/Recipe.java
@@ -1,5 +1,6 @@
 package pw.springdev.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -32,25 +33,27 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
-        joinColumns = @JoinColumn(name = "recipe_id"),
-        inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
-
-    @Enumerated(value = EnumType.STRING)
-    private Difficulty difficulty;
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
---
+++ src/main/java/pw/springdev/services/RecipeService.java
@@ -0,0 +1,16 @@
+package pw.springdev.services;
+
+import org.springframework.stereotype.Service;
+import pw.springdev.domain.Recipe;
+import pw.springdev.repositories.RecipeRepository;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by FedorD on 2020-04-04
+ */
+@Service
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+}
