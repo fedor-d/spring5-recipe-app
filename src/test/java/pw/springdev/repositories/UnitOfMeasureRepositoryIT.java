10ae024fe876cfd001e7f0b9034261569213ca5a
FedorD <fedord.dev@gmail.com>
2020-04-06 15:23:04 +0700

    Add simple Spring integration test. Closes #7

---
+++ src/test/java/pw/springdev/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,39 @@
+package pw.springdev.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import pw.springdev.domain.UnitOfMeasure;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// IT - Integration Test
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
