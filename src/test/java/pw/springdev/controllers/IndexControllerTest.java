300baf91a5d2b5bd58975a1eccd0b6a6e438e7c1
FedorD <fedord.dev@gmail.com>
2020-04-06 14:53:38 +0700

    Add simple example of MockMVC. Closes #6

--- src/test/java/pw/springdev/controllers/IndexControllerTest.java
+++ src/test/java/pw/springdev/controllers/IndexControllerTest.java
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import pw.springdev.domain.Recipe;
 import pw.springdev.services.RecipeService;
@@ -34,6 +39,15 @@ class IndexControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("index"));
     }
 
     @Test
