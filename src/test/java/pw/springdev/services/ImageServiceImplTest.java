b1d26fcb9bfbe3b35edc2fc97d475ea8159a285d
FedorD <fedord.dev@gmail.com>
2020-04-18 12:13:12 +0700

    Finish persist images into DB. Resolves #24

---
+++ src/test/java/pw/springdev/services/ImageServiceImplTest.java
@@ -0,0 +1,58 @@
+package pw.springdev.services;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import pw.springdev.domain.Recipe;
+import pw.springdev.repositories.RecipeRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class ImageServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+        //given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+            "Spring Framework Guru".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        //when
+        imageService.saveImageFile(id, multipartFile);
+
+        //then
+        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
+}
